d0945b6f333de5b4567b9ebaa7021c4225f01cf3
adam <athornton@lsst.org>
2026-08-13 16:14:19 -0700

    Rebase patch set

--- images/nginx/rootfs/build.sh
+++ images/nginx/rootfs/build.sh
@@ -18,7 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-export NGINX_VERSION=1.30.2
+export NGINX_VERSION=1.31.3
 
 # Check for recent changes: https://github.com/vision5/ngx_devel_kit/compare/v0.3.3...master
 export NDK_VERSION=v0.3.3
@@ -189,7 +189,7 @@ mkdir --verbose -p "$BUILD_PATH"
 cd "$BUILD_PATH"
 
 # download, verify and extract the source files
-get_src 7df3090907fca3cc0e456d6dc00ceb230da74ea88026ceff0affc29dbbd9ac4c \
+get_src a7657c50811c2d92d9895395e8b873ef60398142c4db21eb647811c38f6dd525 \
         "https://nginx.org/download/nginx-$NGINX_VERSION.tar.gz"
 
 get_src aa961eafb8317e0eb8da37eb6e2c9ff42267edd18b56947384e719b85188f58b \
